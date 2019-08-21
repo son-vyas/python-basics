fc8f2a22e22c9ae98a17205235ca2c63dc668762
Soniya Vyas <svyas@redhat.com>
2019-08-21 11:49:05 +0530

    Factorial program

---
+++ Factorial.py
@@ -0,0 +1,23 @@
+import sys
+
+def fact(n):
+    """ Factorial function
+        to calculate fatorial
+        of number
+    """
+    if n==0:
+        return 1
+    return n * fact(n-1)
+
+def main(n):
+    res = fact(n)
+    print(res)
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(int(sys.argv[1]))
+
+
+
